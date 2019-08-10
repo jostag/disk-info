9b19d7066890ea03031ac1f20693fb20aa6007c7
jostag <35642154+jostag@users.noreply.github.com>
2019-08-10 00:37:28 -0400

    Add files via upload

---
+++ diskInfo.ps1
@@ -0,0 +1,5 @@
+﻿$Disk = Get-WmiObject -Class Win32_logicaldisk -Filter "DeviceID = 'C:'"
+$DiskPartition = $Disk.GetRelated('Win32_DiskPartition')
+$DiskPartition.GetRelationships() | Select-Object -Property __RELPATH
+$DiskDrive = $DiskPartition.getrelated('Win32_DiskDrive')
+$DiskDrive | Select-Object -Property *
